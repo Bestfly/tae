78183555117db44a85071fbe28bcb7ade7c15209
baina <baina@189.cn>
2014-05-25 17:36:07 +0800

    rebuild the mysql for tipi

--- sails/proxies(AUTO_INCREMENT).sql
+++ sails/proxies(AUTO_INCREMENT).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2014 at 08:17 AM
+-- Generation Time: May 04, 2014 at 05:11 PM
 -- Server version: 5.1.38
 -- PHP Version: 5.3.1
 
@@ -27,18 +27,20 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `proxies` (
   `uid` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ipValue` varchar(21) COLLATE utf8_unicode_ci NOT NULL,
+  `ipValue` varchar(21) COLLATE utf8_unicode_ci DEFAULT NULL,
   `line` tinyint(1) DEFAULT NULL,
   `country` char(2) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `region` varchar(28) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `region` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `speed` varchar(255) COLLATE utf8_unicode_ci,
   `fatchHit` int(11) DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL,
   `effect` tinyint(1) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`ipValue`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ipValue` (`ipValue`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `proxies`
