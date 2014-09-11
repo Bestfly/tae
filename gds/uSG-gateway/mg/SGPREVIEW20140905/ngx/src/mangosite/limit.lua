29a3479e8f788fcb0abf2f7575eaf1144d5ab23f
baina <baina@189.cn>
2014-09-12 00:31:52 +0800

    uSG-gateway update
    add limit with website.

---
+++ gds/uSG-gateway/mg/SGPREVIEW20140905/ngx/src/mangosite/limit.lua
@@ -0,0 +1,59 @@
+-- Jijilu <huangqi@rhomobi.com> 20140904 (v0.5.6)
+-- License: same to the Lua one
+-- TODO: copy the LICENSE file
+-------------------------------------------------------------------------------
+-- begin of the idea : http://rhomobi.com/topics/
+-- Service Gateway
+package.path = "/data/sgcore/ngx/lib/?.lua;";
+local redis = require "resty.redis"
+local http = require "resty.http"
+-- ready to connect to master redis.
+local red, err = redis:new()
+if not red then
+	ngx.say("failed to instantiate redis: ", err)
+	return
+end
+-- lua socket timeout
+-- Sets the timeout (in ms) protection for subsequent operations, including the connect method.
+red:set_timeout(2000) -- 1 sec
+-- nosql connect
+local ok, err = red:connect("127.0.0.1", 6399)
+if not ok then
+	ngx.say("failed to connect redis: ", err)
+	return
+end
+local r, e = red:auth("142ffb5bfa1-cn-jijilu-dg-a75")
+if not r then
+    ngx.say("failed to authenticate: ", e)
+    return
+end
+-- main
+local ips = ngx.shared.iplist;
+local uas = ngx.shared.uagent;
+local updatedt = ips:get("updatedt");
+-- only update banned_ips from Redis once every 20 seconds:
+if updatedt == nil or updatedt < ( ngx.now() - 20 ) then
+    local updatedips, err = red:smembers("banned:ips");
+    if err then
+		ngx.log(ngx.WARN, "Redis read error retrieving banned_ips: " .. err);
+	else
+		-- replace the locally stored banned_ips with the updated values:
+		ips:flush_all();
+		-- insert the ips into the shared dict:
+		for idx, banned_ip in ipairs(updatedips) do
+        	ips:set(banned_ip, true);
+		end
+		ips:set("updatedt", ngx.now());
+	end
+end
+if ips:get(ngx.var.remote_addr) then
+	ngx.log(ngx.WARN, "Banned IP detected and refused access: " .. ngx.var.remote_addr);
+	return ngx.exit(ngx.HTTP_FORBIDDEN);
+end
+-- put it into the connection pool of size 100,
+-- with 10 seconds max idle time
+local ok, err = red:set_keepalive(10000, 500)
+if not ok then
+    ngx.say("failed to set keepalive: ", err)
+    return
+end
---
+++ gds/uSG-gateway/mg/SGPREVIEW20140905/ngx/src/usgateway/init.lua
@@ -0,0 +1,24 @@
+-- buyhome <huangqi@rhomobi.com> 20140730 (v0.5.1)
+-- License: same to the Lua one
+-- TODO: copy the LICENSE file
+-------------------------------------------------------------------------------
+-- begin of the idea : http://rhomobi.com/topics/
+-- Service Gateway
+local sg = ngx.shared.sgcore;
+sg:flush_all();
+-- load mysql forward data into kvdb
+local luasql = require "luasql.mysql"
+local env = assert(luasql.mysql())
+-- base_flights_city
+local con = assert (env:connect("servicegate", "srvg", "srvg_pwd_0819", "10.10.6.59", 3306))
+-- local con = assert (env:connect("biyifei_base", "rhomobi_dev", "b6x7p6b6x7p6", "localhost", 3306))
+con:execute("SET NAMES utf8")
+-- local sqlcmd = "SELECT `serviceName`, `serviceUrl` FROM `tbl_service_map` WHERE `typeId` = '2'";
+local sqlcmd = "SELECT `serviceName`, `serviceUrl` FROM `tbl_service_map`";
+local cur = assert (con:execute(sqlcmd))
+local row = cur:fetch ({}, "a")
+while row do
+	sg:set(row.serviceName, row.serviceUrl)
+	row = cur:fetch (row, "a")
+end
+cur:close();
